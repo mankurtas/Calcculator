28eddf65d69d5089629f1347576278ad4b06cbd5
mankurtas <povis.vasia@gmail.com>
2022-08-24 19:32:04 +0300

    add Class selectros lisiners some methodes

--- script.js
+++ script.js
@@ -27,3 +27,105 @@ kai visas funkcionalumas veikia, CSS faile gražinam UI, rekomenduoju naudot gri
 
 //! nebūtina sekti rekomendacijų, nes galimų sprendimų yra n, bet surašiau, kad padėt, jei išvis nėra minčių;
 //! geriau skirkit pradžioj šiek tiek laiko pagalvot bendrai bendram planui aptart ir gal savo flow sugalvosit
+
+class Calculator {
+  constructor() {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = "";
+  }
+
+  updateDisplay() {}
+  clear() {}
+  delete() {}
+  appendNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
+  compute() {
+    let result;
+    switch (this.operation) {
+      case "+":
+        result = this.previousOperand + this.currentOperand;
+        break;
+      case "-":
+        result = this.previousOperand - this.currentOperand;
+        break;
+      case "*":
+        result = this.previousOperand * this.currentOperand;
+        break;
+      case "÷":
+        result = this.previousOperand / this.currentOperand;
+        break;
+    }
+    this.currentOperand = result;
+  }
+  chooseOperation(operation) {
+    if (this.currentOperand === "") return;
+
+    if (this.previousOperand !== "") {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
+}
+
+const numberButtons = document.querySelectorAll("[data-number]");
+
+const operationButtons = document.querySelectorAll("[data-operation]");
+
+const equalsButton = document.querySelector("[data-equals]");
+
+const deleteButton = document.querySelector("[data-delete]");
+
+const allClearButton = document.querySelector("[data-all-clear]");
+
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+
+    calculator.updateDisplay();
+  });
+});
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+
+    calculator.updateDisplay();
+  });
+});
+
+equalsButton.addEventListener("click", (button) => {
+  calculator.compute();
+
+  calculator.updateDisplay();
+});
+
+//   allClearButton.addEventListener('click', button => {
+
+//     calculator.clear()
+
+//     calculator.updateDisplay()
+
+//   })
+
+//   deleteButton.addEventListener('click', button => {
+
+//     calculator.delete()
+
+//     calculator.updateDisplay()
+
+//   })
